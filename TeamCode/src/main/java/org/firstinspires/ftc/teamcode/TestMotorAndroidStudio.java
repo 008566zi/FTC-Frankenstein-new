d502c9c556fae44bff7edec393411a8ab9ae4ed4
Rohit Pandravada <cododragonsftc@gmail.com>
2026-08-30 16:54:50 -0700

    test comment to RohitBranch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorAndroidStudio.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorAndroidStudio.java
@@ -77,6 +77,11 @@ public class TestMotorAndroidStudio extends LinearOpMode {
     @Override
     public void runOpMode() {
 //MATTHEW IS SO COOL
+
+        //matthew is a matthew
+        //matthew yang
+        //hi im matthew
+
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
         frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
